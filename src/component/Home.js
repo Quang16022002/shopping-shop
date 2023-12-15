e488361cf03f91bb4b6134b71401a93a54af7361
Nguyen Dinh Quang <20010818@st.phenikaa-uni.edu.vn>
2023-12-15 22:17:58 +0700

    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
     };
 
     return (
